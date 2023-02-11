45e41d3fb09aa66165d69f2b4eb3cb7946fc6d1f
AidenBeresford <aidenberes@gmail.com>
2023-02-10 22:14:58 -0600

    Finished code for RGB to Hexadecimal

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::io;
 
+<<<<<<< HEAD
 struct Color(u8, u8, u8);
 
 fn main() {
     
+=======
+struct Color8(u8, u8, u8);
+
+fn main() {
+>>>>>>> db02ca0 (Finished code for RGB to Hexadecimal)
     let mut red_str = String::new();
     let mut blu_str = String::new();
     let mut grn_str = String::new();
@@ -27,6 +33,7 @@ fn main() {
     println!("GREEN-8: ");
 
     io::stdin()
+<<<<<<< HEAD
         .read_line(&mut gre_str)
         .expect("Failed to read stdin");
 
@@ -35,3 +42,23 @@ fn main() {
     user_color = Color(red, blu, gre);
 
 }
+=======
+        .read_line(&mut grn_str)
+        .expect("Failed to read stdin");
+
+    let grn = &grn_str.trim().parse::<u8>().unwrap();
+
+
+
+    println!("Hexadecimal Color Code: {}", rgb_2_hex(*red, *grn, *blu));
+
+}
+
+fn rgb_2_hex(r: u8, g: u8, b: u8) -> String  {
+    let mut hex_str = String::from("");
+
+    hex_str.push_str(&hex::encode(vec![r, g, b]));
+
+    return hex_str;
+}
+>>>>>>> db02ca0 (Finished code for RGB to Hexadecimal)
